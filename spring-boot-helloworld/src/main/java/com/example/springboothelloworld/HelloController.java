ce7d6f688762d3281895070d796887e2e03c939b
HyeJung Shin <hyejungshin7@gamil.com>
2019-08-23 20:43:37 +0900

    Controller, Servlet, Filter, Listener

---
+++ spring-boot-helloworld/src/main/java/com/example/springboothelloworld/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.springboothelloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    private Map<String, Object> result = new HashMap<>();
+
+    @RequestMapping("/hello")
+    public Map<String, Object> hello() {
+        result.put("name", "Stephen");
+        result.put("city", "San Jose");
+        return result;
+    }
+}
---
+++ spring-boot-helloworld/src/main/java/com/example/springboothelloworld/SpringBootHelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.example.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class SpringBootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworldApplication.class, args);
+	}
+
+}
