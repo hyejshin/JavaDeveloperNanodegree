fb0d27461fe3e06cfa07a45f8fec9c82d3c9ad88
HyeJung Shin <hyejungshin7@gamil.com>
2019-08-31 14:26:35 +0900

    GlobalExceptionHandler

--- spring-boot-exception/src/main/java/com/example/springbootexception/controller/UserController.java
+++ spring-boot-exception/src/main/java/com/example/springbootexception/controller/UserController.java
@@ -14,13 +14,13 @@ public class UserController {
         return "add";
     }
 
-    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+    /*@ExceptionHandler(value={java.lang.ArithmeticException.class})
     public ModelAndView handlerArithmeticException(Exception e) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("exception", e.toString());
         modelAndView.setViewName("mathError");
         return modelAndView;
-    }
+    }*/
 
     @RequestMapping("/update")
     public String update() {
@@ -29,11 +29,11 @@ public class UserController {
         return "update";
     }
 
-    @ExceptionHandler(value={java.lang.NullPointerException.class})
+    /*@ExceptionHandler(value={java.lang.NullPointerException.class})
     public ModelAndView handlerNullPointerException(Exception e) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("exception", e.toString());
         modelAndView.setViewName("nullPointerError");
         return modelAndView;
-    }
+    }*/
 }
