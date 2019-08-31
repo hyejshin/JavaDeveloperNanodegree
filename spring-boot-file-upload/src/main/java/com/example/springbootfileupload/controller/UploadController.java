9d2e7c5696a3b0588b568e7e2b42a2d1bcca625a
HyeJung Shin <hyejungshin7@gamil.com>
2019-08-31 16:14:52 +0900

    upload files, thymeleaf

--- spring-boot-file-upload/src/main/java/com/example/springbootfileupload/controller/UploadController.java
+++ spring-boot-file-upload/src/main/java/com/example/springbootfileupload/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.example.springbootfileupload.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,20 +10,23 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 public class UploadController {
     Map<String, Object> result = new HashMap<>();
     /// Receive message
     @RequestMapping("/uploadFile")
-    public Map<String, Object> upload(@RequestParam("attach") MultipartFile file) throws IOException {
+    public String  uploadFile(@RequestParam("attach") MultipartFile file) throws IOException {
         // File info
         System.out.println("File name = "  + file.getOriginalFilename());
         System.out.println("File type = " + file.getContentType());
 
         // Save to disk
         // file path example 1) Windows c:/, 3) Mac ~/Documents/
-        String filePath = "~/Documents/";
+        String filePath = "C:/Users/shins/Documents/";
+        System.out.println(filePath + file.getOriginalFilename());
         file.transferTo(new File(filePath + file.getOriginalFilename()));
         result.put("Success", true);
-        return result;
+        return "success";
     }
+
 }
