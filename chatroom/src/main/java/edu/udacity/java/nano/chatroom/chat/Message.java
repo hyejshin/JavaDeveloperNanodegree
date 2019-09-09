00def2fd8b4d11d368a244f4ced9ddabe66698df
HyeJung Shin <hyejungshin7@gamil.com>
2019-09-09 20:26:50 +0900

    websocket chatroom

---
+++ chatroom/src/main/java/edu/udacity/java/nano/chatroom/chat/Message.java
@@ -0,0 +1,62 @@
+package edu.udacity.java.nano.chatroom.chat;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * WebSocket message model
+ */
+public class Message {
+    // TODO: add message model.
+    private String type;
+    private String username;
+    private String content;
+    private int onlineCount;
+
+    public Message() {
+
+    }
+
+    public Message(String type, String username, String content, int onlineCount) {
+
+        this.type = type;
+        this.username = username;
+        this.content = content;
+        this.onlineCount = onlineCount;
+    }
+
+    public static String jsonConverter(String type, String username, String message, int onlineCount) {
+        return JSON.toJSONString(new Message(type, username, message, onlineCount));
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
+}
---
+++ chatroom/src/main/java/edu/udacity/java/nano/chatroom/chat/WebSocketChatServer.java
@@ -0,0 +1,82 @@
+package edu.udacity.java.nano.chatroom.chat;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+@ServerEndpoint("/chat")
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+    private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+
+    private static void sendMessageToAll(String msg) {
+        //TODO: add send message method.
+        onlineSessions.forEach((id, session) -> {
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        //TODO: add on open connection.
+        if(onlineSessions.containsKey(session.getId())) {return;}
+
+        onlineSessions.put(session.getId(), session);
+        sendMessageToAll(Message.jsonConverter("ENTER", "", "", onlineSessions.size()));
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session, String jsonStr) {
+        //TODO: add send message.
+        Message msg = JSON.parseObject(jsonStr, Message.class);
+        sendMessageToAll(Message.jsonConverter("SPEAK", msg.getUsername(), msg.getContent(), onlineSessions.size()));
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) {
+        //TODO: add close connection.
+        onlineSessions.remove(session.getId());
+        sendMessageToAll(Message.jsonConverter("QUIT", "", "", onlineSessions.size()));
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+}
