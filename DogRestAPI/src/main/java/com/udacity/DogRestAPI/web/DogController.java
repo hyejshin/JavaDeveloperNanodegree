867882574580c00239f9edf781e5499c57162142
HyeJung Shin <hyejungshin7@gamil.com>
2019-09-20 15:52:49 +0900

    swagger

--- DogRestAPI/src/main/java/com/udacity/DogRestAPI/web/DogController.java
+++ DogRestAPI/src/main/java/com/udacity/DogRestAPI/web/DogController.java
@@ -2,15 +2,24 @@ package com.udacity.DogRestAPI.web;
 
 import com.udacity.DogRestAPI.entity.Dog;
 import com.udacity.DogRestAPI.service.DogService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(code=400, message = "This is a bad request, please follow the API documentation for the proper request format."),
+        @ApiResponse(code=401, message = "Due to security constraints, your access request cannot be authorized. "),
+        @ApiResponse(code=500, message = "The server is down. Please make sure that the Location microservice is running.")
+})
+
 public class DogController {
     private DogService dogService;
 
